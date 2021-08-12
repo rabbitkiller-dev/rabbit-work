bdd85f60f9c98c7eb0550af0a862b741474e27da
rabbitkiller <243249439@qq.com>
2021-08-12 15:40:03 +0800

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,13 @@
+/// <reference path="./deployctl.d.ts" />
+
+addEventListener("fetch", (event: FetchEvent) => {
+  const response = new Response([
+{name: 'name'},
+{name: 'name1'},
+{name: 'name2'},
+{name: 'name3'}
+], {
+    headers: { "content-type": "application/json" },
+  });
+  event.respondWith(response);
+});
