9e4453300e0209c0a4422fb68edf730414b29ea8
rabbitkiller <243249439@qq.com>
2021-08-12 15:41:19 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,12 +1,12 @@
 /// <reference path="./deployctl.d.ts" />
 
 addEventListener("fetch", (event: FetchEvent) => {
-  const response = new Response([
+  const response = new Response(JSON.stringify([
 {name: 'name'},
 {name: 'name1'},
 {name: 'name2'},
 {name: 'name3'}
-], {
+]), {
     headers: { "content-type": "application/json" },
   });
   event.respondWith(response);
